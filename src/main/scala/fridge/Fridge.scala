fcafb39be743b5acbfb8d7a003707088fc526e05
Paco Benitez <paco.benitez@sfd.gov.sa>
2022-02-11 15:00:52 +0100

    setCurrentDate

--- src/main/scala/fridge/Fridge.scala
+++ src/main/scala/fridge/Fridge.scala
@@ -1,14 +1,31 @@
 package fridge
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+object Fridge {
+    val Formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+}
+
 class Fridge {
 
-    def setCurrentDate() = ???
-    def signalFridgeDoorOpened() = ???
-    def signalFridgeDoorClosed() = ???
+    var currentDate: LocalDate = LocalDate.now
+
+
+    def signalFridgeDoorOpened() = {}
+    def signalFridgeDoorClosed() = {}
     def scanAddedItem(name: String, expiry: String, condition: String) = ???
     def scanRemovedItem(name: String) = ???
-    def simulateDayOver() = ???
     def showDisplay(): String = ???
-    def setCurrentDate(date: String) = ???
-  
+
+
+    def simulateDayOver() = {
+        currentDate = currentDate.plusDays(1)
+    }
+
+    def setCurrentDate(date: String) = {
+        currentDate = LocalDate.parse(date, Fridge.Formatter)
+    }
+
+    def getTempCurrentDate(): LocalDate = currentDate
 }
