07f9d3a8d37a206fff4c00c06348158e5a76d019
Paco Benitez <paco.benitez@sfd.gov.sa>
2022-02-11 14:21:15 +0100

    initial

---
+++ src/main/scala/fridge/Fridge.scala
@@ -0,0 +1,14 @@
+package fridge
+
+class Fridge {
+
+    def setCurrentDate() = ???
+    def signalFridgeDoorOpened() = ???
+    def signalFridgeDoorClosed() = ???
+    def scanAddedItem(name: String, expiry: String, condition: String) = ???
+    def scanRemovedItem(name: String) = ???
+    def simulateDayOver() = ???
+    def showDisplay(): String = ???
+    def setCurrentDate(date: String) = ???
+  
+}
---
+++ src/test/scala/fridge/ShowDisplaySpec.scala
@@ -0,0 +1,61 @@
+package fridge
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class ShowDisplaySpec extends AnyFlatSpec with Matchers {
+  "Fridge" should "run simulation of three days" in {
+    val fridge = new Fridge()
+
+    fridge.setCurrentDate("18/10/2021")
+
+    fridge.signalFridgeDoorOpened()
+    fridge.scanAddedItem(name = "Milk", expiry= "21/10/21", condition= "sealed")
+    fridge.scanAddedItem(name= "Cheese", expiry= "18/11/21", condition= "sealed")
+    fridge.scanAddedItem(name= "Beef", expiry= "20/10/21", condition= "sealed")
+    fridge.scanAddedItem(name= "Lettuce", expiry= "22/10/21", condition= "sealed")
+    fridge.signalFridgeDoorClosed()
+
+    fridge.simulateDayOver()
+
+    fridge.signalFridgeDoorOpened()
+    fridge.signalFridgeDoorClosed()
+
+    fridge.signalFridgeDoorOpened()
+    fridge.signalFridgeDoorClosed()
+
+    fridge.signalFridgeDoorOpened()
+    fridge.scanRemovedItem(name= "Milk")
+    fridge.signalFridgeDoorClosed()
+
+    fridge.signalFridgeDoorOpened()
+    fridge.scanAddedItem(name= "Milk", expiry= "26/10/21", condition= "opened")
+    fridge.scanAddedItem(name= "Peppers", expiry= "23/10/21", condition= "opened")
+    fridge.signalFridgeDoorClosed()
+
+    fridge.simulateDayOver()
+
+    fridge.signalFridgeDoorOpened()
+    fridge.scanRemovedItem(name= "Beef")
+    fridge.scanRemovedItem(name= "Lettuce")
+    fridge.signalFridgeDoorClosed()
+
+    fridge.signalFridgeDoorOpened()
+    fridge.scanAddedItem(name= "Lettuce", expiry= "22/10/21", condition= "opened")
+    fridge.signalFridgeDoorClosed()
+
+    fridge.signalFridgeDoorOpened()
+    fridge.signalFridgeDoorClosed()
+
+    fridge.simulateDayOver()
+
+    fridge.showDisplay() should be(
+      """EXPIRED: Milk
+Lettuce: 0 days remaining
+Peppers: 1 day remaining
+Cheese: 31 days remaining"""
+    )
+
+
+  }
+}
